d5b8bafe7f2cace619f2dd2b0b0dc7fe72111632
Darnol <32634736+Darnol@users.noreply.github.com>
2024-10-06 09:49:49 +0200

    Done fizz_buzz

--- A1_fizz_buzz/src/main.rs
+++ A1_fizz_buzz/src/main.rs
@@ -1,25 +1,105 @@
 
-use regex::Regex;
-use std::io::{self, Write}; // Import Write to use flush()
+use std::io::Write; // Import Write to use flush()
 
 fn main() {
-    let mut user_input: String = String::new();
-    
-    println!("Please enter four positive integer numbers (m n x y) separated by one or more blank spaces or type quit.");
 
-    // CHATGPT recomends to flush before reading
-    let _ = std::io::stdout().flush(); // Ensures the prompt is printed before waiting for input
-
-    let _ = std::io::stdin().read_line(&mut user_input).unwrap();
+    'input_loop: loop {
         
-    // CHATGPT proposed this trim function so that match does not need to care about newline after read_line()
-    let trimmed_input = user_input.trim();
-    
-    match trimmed_input {
-        "quit" => {
+        let mut user_input: String = String::new();
+        println!("Please enter four positive integer numbers (m n x y) separated by one or more blank spaces or type 'quit'.");
+
+        // CHATGPT recomends to flush before reading
+        let _ = std::io::stdout().flush().expect("Failed to flush stdout"); // Ensures the prompt is printed before waiting for input
+        let _ = std::io::stdin().read_line(&mut user_input).expect("Failed to read line");
+            
+        // CHATGPT proposed this trim function so that match does not need to care about newline after read_line()
+        // Also the book chapter 2 uses this approach
+        let trimmed_input: &str = user_input.trim();
+
+        // Catch 'quit' early
+        if trimmed_input == "quit" {
             println!("You have chosen to quit the program.");
-            return;
+            break 'input_loop; // terminate the loop alltogether
+        }
+
+        // Now, we want to split the trimmed_input into a vector of strings, split by whitespaces
+        let split_input: Vec<&str> = trimmed_input.split_whitespace().collect();
+
+        // Use match expression (most Rustacean way)
+        match split_input[..] {
+            // Try to parse to u32 and apply condition "First number must be smaller than second" and "Thrid and fourth must differ"
+            [m,n,x,y] => {
+                
+                // Parse the strings to u32
+                let m: u32 = match check_input_integer(m, "First number is not a valid positive integer. Try again.") {
+                    Some(value) => value,
+                    None => continue,
+                };
+                let n: u32 = match check_input_integer(n, "Second number is not a valid positive integer. Try again.") {
+                    Some(value) => value,
+                    None => continue,
+                };
+                let x: u32 = match check_input_integer(x, "Thrid number is not a valid positive integer. Try again.") {
+                    Some(value) => value,
+                    None => continue,
+                };
+                let y: u32 = match check_input_integer(y, "Fourth number is not a valid positive integer. Try again.") {
+                    Some(value) => value,
+                    None => continue,
+                };
+
+                // Compare first smaller than second
+                if m >= n {
+                    println!("First number must be smaller than the second. Try again.");
+                    continue;
+                }
+
+                // Compare third and fourth differ
+                if x == y {
+                    println!("Third and fourth number must differ. Try again.");
+                    continue;
+                }
+
+                // We use a Vec<&str> to dynamically add the result strings and finally call join on it
+                // NOTE: &str is not appropriate, because we borrow the i in the loop, and &str would only borrow the reference
+                let mut result_string_vector: Vec<String> = Vec::new();
+                for i in m..=n {
+                    
+                    // Do the fizz buzz
+                    // Most RUST like is to use match again
+                    match (i % x, i % y) {
+                        (0, 0) => result_string_vector.push("FizzBuzz".to_string()),
+                        (0, _) => result_string_vector.push("Fizz".to_string()),
+                        (_, 0) => result_string_vector.push("Buzz".to_string()),
+                        (_, _) => result_string_vector.push(i.to_string()),
+                    }
+                }
+
+                // print the final result
+                println!("{}", result_string_vector.join(", "));
+
+            },
+            [] => println!("Your input is empty. Try again."),
+            _ => println!("Your input is not of length four. Try again."),
+        }
+
+
+    }
+}
+
+fn check_input_integer(input: &str, msg: &str) -> Option<u32> {
+    match input.parse() {
+        Ok(num) => match num {
+            // Also disallow 0
+            0 => {
+                println!("{}", msg);
+                None // Return None for the Option return value
+            },
+            _ => Some(num), // Return Some(num) for the Option return value
         },
-        _ => println!("This is your input: {}", user_input),
+        Err(_) => {
+            println!("{}", msg);
+            None // Return None for the Option return value
+        }
     }
 }
