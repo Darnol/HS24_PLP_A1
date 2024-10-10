6af044b3f9718c17eff56ede81ac2dd09bb778a5
Darnol <32634736+Darnol@users.noreply.github.com>
2024-10-10 13:19:03 +0200

    Upate quicksort with comments and wrapper for quicksort

--- A1_T4_1_quicksort/src/main.rs
+++ A1_T4_1_quicksort/src/main.rs
@@ -5,60 +5,59 @@ use std::cmp::PartialOrd;
 // The lomuto algorithm has a high time complexity if the array is already sorted.
 // A cleverer pivot chosing schema (for example middle of three random elements) could be implemented to improve performance
 // Regarding the Rust specific implementation: I use array slices, they have the convenient swap method.
-// By using arrays and mutable references, the whole operation should be relatively memory efficient I think
+// By using arrays and mutable references, the whole operation should be relatively memory efficient I think, since not much data is copied in memory.
 
 fn main() {
 
     let mut array_to_sort_int: [isize; 5] = [5,4,3,2,1];
     println!("Sort int array {:?}", array_to_sort_int);
-    let right = (array_to_sort_int.len() as isize) - 1;
-    quicksort(&mut array_to_sort_int, 0, right);
+    quicksort(&mut array_to_sort_int);
     println!("Sorted array int {:?}", array_to_sort_int);
 
     let mut array_to_sort_float: [f64; 5] = [4.2,6.77,1.34,8.94,6.34];
     println!("Sort float array {:?}", array_to_sort_float);
-    let right = (array_to_sort_float.len() as isize) - 1;
-    quicksort(&mut array_to_sort_float, 0, right);
+    quicksort(&mut array_to_sort_float);
     println!("Sorted array float {:?}", array_to_sort_float);
 
     let mut array_to_sort_str: [&str; 5] = ["apple", "banana", "apostrophe", "xavier", "orange"];
     println!("Sort str array {:?}", array_to_sort_str);
-    let right = (array_to_sort_str.len() as isize) - 1;
-    quicksort(&mut array_to_sort_str, 0, right);
+    quicksort(&mut array_to_sort_str);
     println!("Sorted array str {:?}", array_to_sort_str);
 
     // Interesting: Tuple in Rust implements PartialOrd if all elements in the tuple implement PartialOrd
     let mut array_to_sort_tuple: [(isize, &str); 5] = [(5, "apple"), (2, "banana"), (3, "apostrophe"), (2, "xavier"), (1, "orange")];
     println!("Sort tuple array {:?}", array_to_sort_tuple);
-    let right = (array_to_sort_tuple.len() as isize) - 1;
-    quicksort(&mut array_to_sort_tuple, 0, right);
+    quicksort(&mut array_to_sort_tuple);
     println!("Sorted array tuple {:?}", array_to_sort_tuple);
 
     // Special cases
     let mut array_empty: [isize; 0] = [];
     println!("Sort empty array {:?}", array_empty);
-    let right: isize = (array_empty.len() as isize) - 1;
-    quicksort(&mut array_empty, 0, right);
+    quicksort(&mut array_empty);
     println!("Sorted empty array {:?}", array_empty);
 
     let mut array_len_1: [isize; 1] = [100];
     println!("Sort len 1 array {:?}", array_len_1);
-    let right: isize = (array_len_1.len() as isize) - 1;
-    quicksort(&mut array_len_1, 0, right);
+    quicksort(&mut array_len_1);
     println!("Sorted len 1 array {:?}", array_len_1);
 
     let mut array_len_2_identical: [isize; 2] = [200, 200];
     println!("Sort len 2 identical array {:?}", array_len_2_identical);
-    let right: isize = (array_len_2_identical.len() as isize) - 1;
-    quicksort(&mut array_len_2_identical, 0, right);
+    quicksort(&mut array_len_2_identical);
     println!("Sorted len 2 identical array {:?}", array_len_2_identical);
 
 }
 
+/// This is the "wrapper" entry point for any quicksort application. Does not require to specify left and right. Otherwise calling the function in main is a bit cumbersome
+/// I got this idea from here: https://www.alxolr.com/articles/quicksort-algorithm-implemented-in-rust-language-with-an-easy-to-remember-partitioning-function
+fn quicksort<T: PartialOrd>(array: &mut [T]) {
+    _quicksort(array, 0, (array.len() as isize) - 1);
+}
+
 /// Any type T that implements the PartialOrd trait can be sorted using this function
 /// I implemented a simple Lomuto quicksort algorithm, where the pivot is the rightmost element in the array
-/// NOTE: Rust requires us to specify usize for any indexing variable. This leads to some type casting problems, because most quicksort implementations use negative indices at some point
-fn quicksort<T: PartialOrd>(array: &mut [T], left: isize, right: isize) {
+/// NOTE: Rust requires us to specify usize for any indexing variable. This leads to some type casting problems, because most quicksort implementations use negative indices at some point.
+fn _quicksort<T: PartialOrd>(array: &mut [T], left: isize, right: isize) {
 
     // println!("quicksort: Array {:?}, left {} right {}", array, left, right);
 
@@ -77,10 +76,10 @@ fn quicksort<T: PartialOrd>(array: &mut [T], left: isize, right: isize) {
     
     // Recursively sort the left half, but ONLY IF pivot_index is greater than 0. Otherwise we have a isize/usize casting problem
     if pivot_index > 0 {
-        quicksort(array, left as isize, (pivot_index-1) as isize);
+        _quicksort(array, left as isize, (pivot_index-1) as isize);
     }
     // partition the right half
-    quicksort(array, (pivot_index+1) as isize, right as isize);
+    _quicksort(array, (pivot_index+1) as isize, right as isize);
     
 }
 
---
+++ A1_T4_1_quicksort/target/debug/A1_T4_1_quicksort.d
@@ -0,0 +1,1 @@
+/Users/dominik/Desktop/HS24_repos/HS24_PLP_A1/A1_T4_1_quicksort/target/debug/A1_T4_1_quicksort: /Users/dominik/Desktop/HS24_repos/HS24_PLP_A1/A1_T4_1_quicksort/src/main.rs
